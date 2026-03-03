e662e3d74ebe378f535b7cd94d68c3c568b6e1e9
sebdied <sebastian.diedrich@t-online.de>
2026-03-03 11:27:33 +0800

    Analyzing each column and starting to correct missing data

---
+++ src/utils.py
@@ -0,0 +1,37 @@
+# Standard imports
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+# Distribution plotting
+def plot_distributions(df: pd.DataFrame, column: str, figsize=(15, 5)):
+    # Plot distributions
+    plt.figure(figsize=(8, 5))
+    sns.histplot(df[column], kde=True)
+    plt.title(f'Distribution of {column}')
+    plt.xlabel('Value')
+    plt.ylabel('Frequency')
+    plt.show()
+
+
+# Analyse data distribution and neg. values of a column
+def analyze_col(df: pd.DataFrame, col_name: str, target_dtype_is_number: bool):
+    print(f"# Column name: {col_name}")
+    if col_name in df.columns:
+        # Types in column
+        print(f'Types found ({col_name}): {df[col_name].dtypes}')
+        # Try to convert dtype to float to check for neg. values
+        if target_dtype_is_number is True:
+            # Convert the column to numeric, handling errors
+            # Replace ',' with '.' if necessary and convert to float
+            col_to_float = f'{col_name}_to_float'
+            df[col_to_float] = df[col_name].str.replace(',', '.', regex=False).astype(float)
+            print(f'Types found ({col_to_float}): {df[col_to_float].dtypes}')
+            # Count of neg. values
+            print(f'Neg. values ({col_to_float}): {(df[col_to_float] < 0).sum()}')
+            # Plot distribution
+            plot_distributions(df, col_to_float, (15, 5))
+        else:
+            # Plot distribution
+            plot_distributions(df, [col_name], (15, 5))
